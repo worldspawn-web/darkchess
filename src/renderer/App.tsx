ff4b678c703631971ada9813245e3ba7ae7549f0
Michael Lozitckii <worldspawn.web@gmail.com>
2025-01-09 23:40:11 +0300

    perf: ⚡️ minor useState

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -1,10 +1,10 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import ChessBoard from './components/ChessBoard';
 import StatsPanel from './components/StatsPanel';
 
 const App: React.FC = () => {
-  const [gameMode, setGameMode] = React.useState<'30M' | '10M'>('30M');
+  const [gameMode, setGameMode] = useState<'30M' | '10M'>('30M');
 
   const handleGameModeChange = (mode: '30M' | '10M') => {
     setGameMode(mode);
